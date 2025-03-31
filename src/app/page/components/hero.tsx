358d096442ae4138888a1e51b2e2e69289ef70cb
Aseem Asthana <aseem@aseem.us>
2025-03-31 09:43:53 -0700

    fixes to Hero component to increase readability

--- src/app/page/components/hero.tsx
+++ src/app/page/components/hero.tsx
@@ -7,10 +7,11 @@ import { NumbersSection } from './Numbers'
 export function Hero() {
   return (
     <div className="relative">
+      <Gradient className="absolute inset-2 bottom-0 rounded-4xl ring-1 ring-black/5 ring-inset" />
       <Container className="relative">
         <Navbar />
         <div className="pt-1 pb-24 sm:pt-24 sm:pb-32 md:pt-24 md:pb-48">
-          <h1 className="font-display text-5xl/[0.9] font-medium tracking-tight text-balance text-gray-950 sm:text-8xl/[0.8] md:text-8xl/[0.8]">
+          <h1 className="font-display text-5xl/[1.1] font-semibold tracking-normal text-balance text-gray-950 sm:text-8xl/[1.05] md:text-8xl/[1.05] drop-shadow-sm">
           Real-Time Sales Forecasts That Stay on Track
           </h1>
           <p className="mt-8 max-w-3xl text-xl/7 font-medium text-gray-950/75 sm:text-2xl/8">
