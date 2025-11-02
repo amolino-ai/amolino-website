76ef2a8957e302a75170a005b18ac6c6fd165877
Aseem Asthana <aseem@aseem.us>
2025-11-02 08:06:10 -0800

    make strict mode explicit

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,7 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   outputFileTracingIncludes: {
     '/**/*': ['./src/app/**/*.mdx'],
