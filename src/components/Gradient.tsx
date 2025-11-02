670568317cb4bbe0b5981869ba6d4baaed1e49d2
Aseem Asthana <aseem@aseem.us>
2025-11-02 08:23:58 -0800

    fix mobile horizontal scrolling problem

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -12,12 +12,15 @@ export function Gradient({ className, ...props }: React.ComponentPropsWithoutRef
   );
 }
 
+// you can do hidden md:block if you want to hide on smaller screens. Add that 
+// to the clxs className below
+
 export function GradientBackground() {
   return (
     <div className="relative mx-auto max-w-7xl">
       <div
         className={clsx(
-          'absolute -top-44 -right-60 h-60 w-[36rem] transform-gpu md:right-0',
+          'absolute -top-44 right-0 md:-right-60 h-60 w-[36rem] transform-gpu',
           'bg-linear-115 from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff]',
           'z-0 rotate-[-10deg] rounded-full blur-3xl',
         )}
